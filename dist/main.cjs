4e2705e39919ab5de5b8c2e89832dcf36e9e8d69
Maysara <Maysara.Elshewehy@gmail.com>
2025-11-29 00:58:00 +0200

    up

---
+++ dist/main.cjs
@@ -0,0 +1,2 @@
+'use strict';function e(o){console.log(o);}var s="Hello, World!";function r(){e(s);}r();//# sourceMappingURL=main.cjs.map
+//# sourceMappingURL=main.cjs.map
---
+++ dist/main.js
@@ -0,0 +1,2 @@
+function e(o){console.log(o);}var s="Hello, World!";function r(){e(s);}r();//# sourceMappingURL=main.js.map
+//# sourceMappingURL=main.js.map
